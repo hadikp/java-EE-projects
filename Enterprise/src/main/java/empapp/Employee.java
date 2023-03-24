b887dc0e937face26b5144cd4dac8a92722e36eb
hadikp <hp20001025@gmail.com>
2023-03-24 22:52:35 +0100

    plus mariadb connection

--- Enterprise/src/main/java/empapp/Employee.java
+++ Enterprise/src/main/java/empapp/Employee.java
@@ -2,7 +2,15 @@ package empapp;
 
 public class Employee {
 
+    private Long id;
     private String name;
+
+    public Employee() {}
+
+    public Employee(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Employee(String name) {
         this.name = name;
--- Enterprise/src/main/java/empapp/EmployeeBean.java
+++ Enterprise/src/main/java/empapp/EmployeeBean.java
@@ -1,7 +1,14 @@
 package empapp;
 
+import javax.annotation.Resource;
 import javax.ejb.Singleton;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,15 +18,37 @@ public class EmployeeBean {
 
     @Inject
     private NameTrimmer nameTrimmer;
-    private List<Employee> employees = Collections.synchronizedList(
-            new ArrayList<>(List.of(new Employee("John Doe"),
-                    new Employee("Jane Done"))));
+
+    @Resource(mappedName = "java:/jdbc/EmployeeDS")
+    private DataSource dataSource;
+
     public List<Employee> findEmployees() {
-        return Collections.unmodifiableList(employees);
+        try (Connection conn = dataSource.getConnection();
+        PreparedStatement st = conn.prepareStatement("Select * from employees");
+        ResultSet rs = st.executeQuery()
+        ) {
+            List<Employee> employees = new ArrayList<>();
+            while (rs.next()){
+                long id = rs.getLong("id");
+                String name = rs.getString("emp_name");
+                Employee employee = new Employee(id, name);
+                employees.add(employee);
+            }
+            return employees;
+
+        } catch (SQLException se){
+            throw new  IllegalStateException("Can not query employees", se);
+        }
     }
 
     public void saveEmployee(String name) {
-        String trimmedName = nameTrimmer.trimName(name);
-        employees.add(new Employee(trimmedName));
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement st = conn.prepareStatement("Insert into employees(emp_name) values (?)")
+        ){
+            st.setString(1, name);
+            st.executeUpdate();
+        } catch (SQLException se){
+            throw new  IllegalStateException("Can not query employees", se);
+        }
     }
 }
