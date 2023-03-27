cde179a0c7adeb8968d2e18c4044a2277f0439f0
hadikp <hp20001025@gmail.com>
2023-03-27 22:59:18 +0200

    change to jpa

--- Enterprise/src/main/java/empapp/Employee.java
+++ Enterprise/src/main/java/empapp/Employee.java
@@ -1,8 +1,16 @@
 package empapp;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "emp_name")
     private String name;
 
     public Employee() {}
--- Enterprise/src/main/java/empapp/EmployeeBean.java
+++ Enterprise/src/main/java/empapp/EmployeeBean.java
@@ -1,54 +1,37 @@
 package empapp;
 
+
 import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
+import javax.transaction.Transactional;
 import java.util.List;
+
 
 
 public class EmployeeBean {
 
+    public EmployeeBean() {
+    }
+
     @Inject
     private NameTrimmer nameTrimmer;
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Resource(mappedName = "java:/jdbc/EmployeeDS")
     private DataSource dataSource;
 
     public List<Employee> findEmployees() {
-        try (Connection conn = dataSource.getConnection();
-        PreparedStatement st = conn.prepareStatement("Select * from employees");
-        ResultSet rs = st.executeQuery()
-        ) {
-            List<Employee> employees = new ArrayList<>();
-            while (rs.next()){
-                long id = rs.getLong("id");
-                String name = rs.getString("emp_name");
-                Employee employee = new Employee(id, name);
-                employees.add(employee);
-            }
-            return employees;
-
-        } catch (SQLException se){
-            throw new  IllegalStateException("Can not query employees", se);
-        }
+        return em.createQuery("select e from Employee e", Employee.class).getResultList();
     }
 
+    @Transactional
     public void saveEmployee(String name) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement st = conn.prepareStatement("Insert into employees(emp_name) values (?)")
-        ){
-            st.setString(1, name);
-            st.executeUpdate();
-        } catch (SQLException se){
-            throw new  IllegalStateException("Can not query employees", se);
-        }
+        em.persist(new Employee(name));
     }
 }
