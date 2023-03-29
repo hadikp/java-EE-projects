48a46d1eb6dce1847243a269985050d6ba35496e
hadikp <hp20001025@gmail.com>
2023-03-29 20:45:48 +0200

    first deploy

--- ToDo/Readme.md
+++ ToDo/Readme.md
@@ -1,1 +1,10 @@
 Ez az alkalmazás a Glassfish alkalmazásszerveren fut
+Az alkalmazásunk lebildelése mavennel (a JAR állomány elkészítése):
+```shell
+mvn clean package 
+```
+
+Alkalmazás deploy:
+```shell
+java -jar payara-5.48.1.jar --deploy C:\H\ujratervez2021\SEJavaRaepulo\java-EE-projects\ToDo\target\todo.war --port 8080
+```
--- ToDo/src/main/java/todo/TodoService.java
+++ ToDo/src/main/java/todo/TodoService.java
@@ -1,7 +1,32 @@
 package todo;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class TodoService {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Todo> listTodo(){
+        return em.createQuery("select t from Todo t", Todo.class).getResultList();
+    }
+
+    public Todo findTodoById(Long id){
+        return em.find(Todo.class, id);
+    }
+    public Todo saveTodo(Todo todo){
+        em.persist(todo);
+        return todo;
+    }
+
+    public Todo updateTodo(Todo todo){
+        em.merge(todo);
+        return todo;
+    }
 
 }
