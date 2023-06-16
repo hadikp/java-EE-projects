b6850f705b27aed2e86783519bc7388cc2cd85c3
hadikp <hp20001025@gmail.com>
2023-06-16 22:17:07 +0200

    get all data from deposit endpoint

--- Loan/src/main/java/loan/debtor/DepositRestAssured.java
+++ Loan/src/main/java/loan/debtor/DepositRestAssured.java
@@ -5,6 +5,9 @@ import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
 import static io.restassured.RestAssured.given;
 
 public class DepositRestAssured {
@@ -17,15 +20,23 @@ public class DepositRestAssured {
                         then().contentType(ContentType.JSON).extract().response();
     }
 
-    public void dataFromApi(String endpoint){
+    public void dataFromApi(String endpoint) throws ParseException {
         Response response = doGetRequest(endpoint);
-        String fund = response.jsonPath().getString("fund[0]");
-        String interest = response.jsonPath().getString("interest[0]");
-        String year = response.jsonPath().getString("depositYear[0]");
-        String payment = response.jsonPath().getString("payment[0]");
-        System.out.println("Tőke:" + Integer.valueOf(fund));
-        System.out.println("Kamat:" + Double.valueOf(interest));
-        System.out.println("Pénz lekötésének évei:" + Integer.valueOf(year));
-        System.out.println("Befizetés:" + Integer.valueOf(payment));
+        String fund = response.jsonPath().getString("fund[2]");
+        String interest = response.jsonPath().getString("interest[2]");
+        String year = response.jsonPath().getString("depositYear[2]");
+        String payment = response.jsonPath().getString("payment[2]");
+        String annuity = response.jsonPath().getString("annuity[2]");
+        String annuitiesYear = response.jsonPath().getString("annuitiesYear[2]");
+        String depositInterestByYear = response.jsonPath().getString("depositInterestByYear[2]");
+        String depositInterestByMonth = response.jsonPath().getString("depositInterestByMonth[2]");
+        System.out.println("Tőke: " + Integer.valueOf(fund));
+        System.out.println("Kamat: " + Double.valueOf(interest));
+        System.out.println("Pénz lekötésének évei: " + Integer.valueOf(year));
+        System.out.println("Befizetés: " + Integer.valueOf(payment));
+        System.out.println("Életjáradék: " + DecimalFormat.getNumberInstance().parse(annuity).doubleValue());
+        System.out.println("Pénzgyűjtés évei: " + annuitiesYear);
+        System.out.println("Kamatos kamat évente: " + depositInterestByYear);
+        System.out.println("Kamatos kamat havonta: " + depositInterestByMonth);
     }
 }
--- Loan/src/test/java/loan/debtor/DepositRestAssuredTest.java
+++ Loan/src/test/java/loan/debtor/DepositRestAssuredTest.java
@@ -2,12 +2,14 @@ package loan.debtor;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepositRestAssuredTest {
 
     @Test
-    public void jsonTest(){
+    public void jsonTest() throws ParseException {
         String uri = "http://localhost:8080/loan-calc/api/deposit";
         DepositRestAssured depositRestAssured = new DepositRestAssured();
         depositRestAssured.dataFromApi(uri);
