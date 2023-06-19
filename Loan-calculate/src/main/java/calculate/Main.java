df00908100086b96eb31195c327c5082864889ad
hadikp <hp20001025@gmail.com>
2023-06-19 22:31:54 +0200

    activeMQ message from queue

--- Loan-calculate/src/main/java/calculate/Main.java
+++ Loan-calculate/src/main/java/calculate/Main.java
@@ -1,7 +1,26 @@
 package calculate;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //ActiveMQ végpont
+        String endpoint = "http://localhost:8161/api/jolokia";
+
+        //ActiveMQ lekérdezése
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .param("mbean", "org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=MyQueue")
+                .get(endpoint);
+
+        //Válasz
+        if (response.statusCode() == 200){
+            String responseBody = response.getBody().asString();
+            System.out.println(responseBody);
+        }else {
+            System.out.println("Failed to query ActiveMQ Queue. Status code: " + response.statusCode());
+        }
     }
 }
