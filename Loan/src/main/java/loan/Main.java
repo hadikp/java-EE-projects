49fcf9970b65aa62bded22b54da0da5033cdb051
hadikp <hp20001025@gmail.com>
2023-06-19 17:29:43 +0200

    ActiveMQ message broker

--- Loan/src/main/java/loan/Main.java
+++ Loan/src/main/java/loan/Main.java
@@ -1,10 +1,57 @@
 package loan;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue("MyQueue");
+            String urlString = "http://localhost:8080/loan-calc/deposit";
+            String endpointContent = readEndpointContent(urlString);
+
+            MessageProducer producer = session.createProducer(queue);
+            TextMessage message = session.createTextMessage();
+            message.setText(endpointContent);
+            producer.send(message);
+            session.close();
+            connection.close();
+        } catch (JMSException e){
+            e.printStackTrace();
+        }
+
+    }
+    private static String readEndpointContent(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null){
+                    content.append(line);
+                }
+                reader.close();
+                return content.toString();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
