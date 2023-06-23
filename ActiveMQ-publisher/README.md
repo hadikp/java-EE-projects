cfec31764a649ac6c5231915bd2f10d724d15a6f
hadikp <hp20001025@gmail.com>
2023-06-23 20:04:34 +0200

    ActiveMQ publisher works

---
+++ ActiveMQ-publisher/README.md
@@ -0,0 +1,9 @@
+Az alkalmazások közötti kommunikációt apache-ActiveMQ
+segítségével valósítható meg. Az üzenetbróker indítása:
+```shell
+c:\H\apache-activemq-5.17.4\bin\activemq start
+```
+Az ActiveMQ admin felülete:
+```shell
+http://localhost:8161/admin
+```
--- ActiveMQ-publisher/src/main/java/org/example/Main.java
+++ ActiveMQ-publisher/src/main/java/org/example/Main.java
@@ -1,7 +1,60 @@
 package org.example;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue("PublishQueue");
+            String urlString = "http://localhost:8080/loan-calc/api/deposit";
+            String endpointContent = readEndpointContent(urlString);
+
+            MessageProducer producer = session.createProducer(queue);
+            TextMessage message = session.createTextMessage();
+            message.setText(endpointContent);
+            producer.send(message);
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String readEndpointContent(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+                reader.close();
+                return content.toString();
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 }
