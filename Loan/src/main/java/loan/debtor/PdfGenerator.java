ac32bb5632830b91f03e8dce2f00356a65f77575
hadikp <hp20001025@gmail.com>
2023-07-07 17:54:10 +0200

    pdf chart

---
+++ Loan/src/main/java/loan/debtor/PdfGenerator.java
@@ -0,0 +1,63 @@
+package loan.debtor;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.JPEGFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.util.Matrix;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
+import javax.ejb.Stateless;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Stateless
+public class PdfGenerator {
+
+    public byte[] createPdf() throws IOException {
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage(PDRectangle.A4);
+            page.setRotation(90);
+
+            float pageWidth = page.getMediaBox().getWidth();
+            float pageHeight = page.getMediaBox().getHeight();
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            PDImageXObject chartImage = JPEGFactory.createFromImage(document, createChart((int) pageWidth, (int) pageHeight));
+            contentStream.transform(new Matrix(0, 1, -1, 0, pageWidth, 0));
+            contentStream.drawImage(chartImage, 0, 0);
+            contentStream.close();
+
+            document.addPage(page);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            document.save(byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        }
+    }
+
+    private BufferedImage createChart(int width, int height) {
+        XYChart chart = new XYChartBuilder().xAxisTitle("X").yAxisTitle("Y").width(width).height(height)
+                .theme(Styler.ChartTheme.Matlab).build();
+        XYSeries series = chart.addSeries("Random", null, getRandomNumbers(200));
+        series.setMarker(SeriesMarkers.NONE);
+        return BitmapEncoder.getBufferedImage(chart);
+
+    }
+
+    private double[] getRandomNumbers(int numPoints) {
+        double[] y = new double[numPoints];
+        for (int i = 0; i < y.length; i++) {
+            y[i] = ThreadLocalRandom.current().nextDouble(0, 1000);
+        }
+        return y;
+    }
+}
---
+++ Loan/src/main/java/loan/debtor/PdfReport.java
@@ -0,0 +1,25 @@
+package loan.debtor;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+@Path("/pdf")
+@Stateless
+public class PdfReport {
+
+    @Inject
+    private PdfGenerator pdfGenerator;
+
+    @GET
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response createPdfChart() throws IOException {
+        return Response.ok(pdfGenerator.createPdf(), MediaType.APPLICATION_OCTET_STREAM)
+                .header("Content-Disposition", "attachment; filename=\"simpleChart.pdf\"").build();
+    }
+}
