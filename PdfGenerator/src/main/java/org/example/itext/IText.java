132de1b46a142bc0aec61331e052c3d8d08e2355
hadikp <hp20001025@gmail.com>
2023-07-07 21:58:25 +0200

    pdfGenerator iText

---
+++ PdfGenerator/src/main/java/org/example/itext/IText.java
@@ -0,0 +1,30 @@
+package org.example.itext;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import javax.ejb.Stateless;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+@Stateless
+public class IText {
+
+    public Object createPdf() {
+        try {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("iText.pdf"));
+            document.open();
+            Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLUE);
+            Chunk chunk = new Chunk("Hello world", font);
+            document.add(chunk);
+            document.close();
+        } catch (DocumentException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+
+
+}
