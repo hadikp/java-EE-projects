dbbf5ae0982bd5a47431373da56c5d5b9b019148
hadikp <hp20001025@gmail.com>
2023-07-08 21:37:33 +0200

    iText pdf

---
+++ PdfGenerator/src/main/java/org/example/itext/MakePdf.java
@@ -0,0 +1,25 @@
+package org.example.itext;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+@Path("pdf")
+@Stateless
+public class MakePdf {
+
+    @Inject
+    private Itext pdfGenerator;
+
+    @GET
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response createPdf() throws IOException {
+        return Response.ok(pdfGenerator.createPdf(), MediaType.APPLICATION_OCTET_STREAM)
+                .header("Content-Disposition", "attachment; filename=\"iText.pdf\"").build();
+    }
+}
